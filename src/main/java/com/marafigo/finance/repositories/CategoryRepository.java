c1aba3fa105df657dc509346e5e14b203b40366b
Lucas Marafigo <marafigo.lucas16@gmail.com>
2026-03-26 21:10:40 -0300

    implementado TestConfig

--- src/main/java/com/marafigo/finance/repositories/CategoryRepository.java
+++ src/main/java/com/marafigo/finance/repositories/CategoryRepository.java
@@ -1,7 +1,6 @@
 package com.marafigo.finance.repositories;
 
 import com.marafigo.finance.entities.Category;
-import com.marafigo.finance.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
