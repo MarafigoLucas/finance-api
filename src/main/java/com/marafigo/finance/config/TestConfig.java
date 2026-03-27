aa51d4220301c71799710b3b751d7f014ee7063c
Lucas Marafigo <marafigo.lucas16@gmail.com>
2026-03-27 20:27:18 -0300

    feat: implementadas classes CategoryResourse e CategoryService e os endpoints findAll e findById

--- src/main/java/com/marafigo/finance/config/TestConfig.java
+++ src/main/java/com/marafigo/finance/config/TestConfig.java
@@ -1,6 +1,8 @@
 package com.marafigo.finance.config;
 
+import com.marafigo.finance.entities.Category;
 import com.marafigo.finance.entities.User;
+import com.marafigo.finance.repositories.CategoryRepository;
 import com.marafigo.finance.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -14,6 +16,8 @@ import java.util.Arrays;
 public class TestConfig implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -21,6 +25,11 @@ public class TestConfig implements CommandLineRunner {
         User u2 = new User(null, "Nic","Nic@gmail","Olga10",null);
 
         userRepository.saveAll(Arrays.asList(u1,u2));
+
+        Category c1 = new Category(null,"Mercado",null);
+        Category c2 = new Category(null,"Mercado",null);
+
+        categoryRepository.saveAll(Arrays.asList(c1,c2));
     }
 
 }
