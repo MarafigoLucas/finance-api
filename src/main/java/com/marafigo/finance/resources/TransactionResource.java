2bc211838d1e1022df5436ed2e886fab1b506bdd
Lucas Marafigo <marafigo.lucas16@gmail.com>
2026-04-06 18:57:54 -0300

    feat: implementado busca de transação por Id (findById) no Service e Resource

--- src/main/java/com/marafigo/finance/resources/TransactionResource.java
+++ src/main/java/com/marafigo/finance/resources/TransactionResource.java
@@ -5,6 +5,7 @@ import com.marafigo.finance.services.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +21,10 @@ public class TransactionResource {
     public ResponseEntity<List<Transaction>>findAll(){
         List<Transaction> list= service.findAll();
         return ResponseEntity.ok().body(list);
+    }
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Transaction>findById(@PathVariable Long id){
+        Transaction obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
--- src/main/java/com/marafigo/finance/services/TransactionService.java
+++ src/main/java/com/marafigo/finance/services/TransactionService.java
@@ -3,9 +3,12 @@ package com.marafigo.finance.services;
 import com.marafigo.finance.entities.Transaction;
 import com.marafigo.finance.repositories.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TransactionService {
@@ -15,5 +18,12 @@ public class TransactionService {
     public List<Transaction> findAll(){
         return repository.findAll();
     }
+
+    public Transaction findById(Long id){
+        Optional<Transaction> obj = repository.findById(id);
+        return obj.get();
+    }
+
+
 
 }
